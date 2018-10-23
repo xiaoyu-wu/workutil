2d47199b4393b53daa197a30c3187b7967d454e4
Xiaoyu Wu <xwu@enthought.com>
2018-10-22 23:36:08 -0500

    Get work time by activity for specified date

--- workutil/time_tracking.py
+++ workutil/time_tracking.py
@@ -1,12 +1,33 @@
-import subprocess
+import re
+from subprocess import check_output
 
 
 class TimeTracker(object):
-    def utt_report(self):
-        command = "utt report"
-        subprocess.Popen(command.split())
+
+    def utt_report(self, date_str=''):
+        command = "utt report " + date_str
+        report = check_output(command.split())
+        return report.decode('UTF-8')
+
+    def get_work_time_per_activity(self, date_str=''):
+        report = self.utt_report(date_str)
+        report_lines = report.split('\n')
+        match_index = []
+        activity_line_match = re.compile(r"-+ Activities -+")
+        detail_line_match = re.compile(r"-+ Details -+")
+        for idx, line in enumerate(report_lines):
+            if activity_line_match.match(line) or detail_line_match.match(line):
+                match_index.append(idx)
+        work_time_dict = {}
+        assert len(match_index) == 2
+        for line in report_lines[match_index[0] + 1 : match_index[1]]:
+            line_list = line.split()
+            if len(line_list) == 3:
+                m = re.match(r"\((\d+)h(\d+)\)", line_list[0])
+                work_time_dict[line_list[2]] = tuple(int(i) for i in m.groups())
+        return work_time_dict
 
 
 if __name__ == "__main__":
     tt = TimeTracker()
-    tt.utt_report()
+    result = tt.get_work_time_per_activity()
